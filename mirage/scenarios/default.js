53dc6df0a82cbf5eac3ade9647416987ad02152a
Mikek2252 <Michael.r.kerr1@gmail.com>
2021-01-11 17:48:54 +0000

    add new scenario and comment out team price fixtures (may use in future)

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -7,4 +7,25 @@ export default function (server) {
   // server.createList('post', 10);
 
   server.loadFixtures();
+
+  let weeks = server.createList('week', 10);
+  let teams = server.db.teams;
+  weeks.forEach(week => {
+    teams.forEach(team => {
+      let previousWeekId = week.id - 1;
+      let previousWeekTeamPrice = server.db.teamPrices.findBy({ weekId: previousWeekId, teamId: team.id });
+      let price = previousWeekTeamPrice ? updatePrice(previousWeekTeamPrice.price) : 300;
+      server.create('team-price', {
+        teamId: team.id,
+        weekId: week.id,
+        price: price,
+      })
+    })
+  })
+}
+
+function updatePrice(price, maxChange = 30) {
+  let priceChange = Math.floor(Math.random() * Math.floor(maxChange * 2)) - 30;
+  let newPrice = price + priceChange;
+  return newPrice < 0 ? 0 : newPrice;
 }
